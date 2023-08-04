95639e7a9148d3fdbf705add078010b0fd14424c
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-04 10:58:35 -0700

    testing

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p>Hello admin</p>;
+  return (
+    <div className="p-4">
+      <Button size="icon">Hello</Button>
+    </div>
+  );
 }
