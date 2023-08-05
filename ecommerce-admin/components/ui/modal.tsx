5bff1f8d3c7750a859646c9c4314e8df91c0627e
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-05 14:52:33 -0700

    created modals

--- ecommerce-admin/components/ui/modal.tsx
+++ ecommerce-admin/components/ui/modal.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
 interface ModalProps {
   title: string;
   description: string;
@@ -7,7 +15,29 @@ interface ModalProps {
   onClose: () => void;
   children?: React.ReactNode;
 }
- 
-export const Modal: React.FC<ModalProps>=({title, description, isOpen, onClose, children})=> {
-    const onChange = (open: boolean)
-}
+
+export const Modal: React.FC<ModalProps> = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}) => {
+  const onChange = (open: boolean) => {
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+};
