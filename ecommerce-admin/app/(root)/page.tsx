c0ab0fdda1ed66f55b2126777d8d2e21f9dcbf95
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-05 12:24:09 -0700

    added modal

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,8 +1,18 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Modal } from "@/components/ui/modal";
+
 const SetupPage = () => {
   return (
     <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
+      <Modal
+        title="Test"
+        description="Test Description"
+        isOpen
+        onClose={() => {}}
+      >
+        Children
+      </Modal>
     </div>
   );
 };
