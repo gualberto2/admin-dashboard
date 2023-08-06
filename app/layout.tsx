dee04fa25475ed4914b575a878150a6b71962554
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-05 17:30:55 -0700

    adjusted file paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
