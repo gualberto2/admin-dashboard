fe45ce8402da543c5230bc9cd6433fbd56182092
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-05 20:06:01 -0700

    added navigation for storeId params

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,8 +1,20 @@
 "use client";
 
+import { cn } from "@/lib/utils";
+
+import { useParams, usePathname } from "next/navigation";
+
 export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
-  return <p>Main Navigation</p>;
+  const pathname = usePathname();
+  const params = useParams();
+
+  const routes = [{ href: `/${params.storeId}` }];
+  return (
+    <nav
+      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
+    ></nav>
+  );
 }
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
-import { MainNav } from "./main-nav";
+import { MainNav } from "@/components/main-nav";
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <div className="">This wil be a store switcher</div>
-        <MainNav />
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
         </div>
