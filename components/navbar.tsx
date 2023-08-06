830caa39bf51383efd076dd208c93b72c9f68f2b
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-05 20:02:50 -0700

    structuring nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,19 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
+import { MainNav } from "./main-nav";
 
 const Navbar = () => {
-  return <div>dis a navbar fr</div>;
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4">
+        <div className="">This wil be a store switcher</div>
+        <MainNav />
+        <div className="ml-auto flex items-center space-x-4">
+          <UserButton afterSignOutUrl="/" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
