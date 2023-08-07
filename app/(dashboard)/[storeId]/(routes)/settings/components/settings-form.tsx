a7228aef2b318203d30bd4df19f30c69dd495642
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-07 09:04:54 -0700

    added zodResolver, async submit

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,15 +1,35 @@
 "use client";
 
+import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Separator } from "@/components/ui/separator";
+import { useState } from "react";
 
 interface SettingsFormProps {
   initialData: Store;
 }
 
-const SettingsForm: React.FC<SettingsFormProps> = ({ initalData }) => {
+const formSchema = z.object({ name: z.string().min(1) });
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<SettingsFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  });
+
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
+  };
   return (
     <>
       <div className="flex items-center justify-between">
@@ -18,6 +38,7 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initalData }) => {
           <Trash className="h-4 w-4" />
         </Button>
       </div>
+      <Separator />
     </>
   );
 };
