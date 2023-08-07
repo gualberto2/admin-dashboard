ebc2a80b65d3ca3ac3a795bbf8fdc4b6f156d451
gualberto2 <elijahwilsonnn@gmailcom>
2023-08-07 08:55:42 -0700

    added stylingfor settings

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,5 +1,25 @@
-const SettingsForm = () => {
-  return <div>settings-form</div>;
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ initalData }) => {
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button variant="destructive" size="sm" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default SettingsForm;
