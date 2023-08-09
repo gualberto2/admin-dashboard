b1d1f3eddf52992d4e4df46b9471c6779258669d
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-09 11:25:50 -0700

    added billboards and overview to nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -14,6 +14,16 @@ export function MainNav({
 
   const routes = [
     {
+      href: `/${params.storeId}`,
+      label: "Overview",
+      active: pathname === `/${params.storeId}`,
+    },
+    {
+      href: `/${params.storeId}/billboards`,
+      label: "Billboards",
+      active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
       href: `/${params.storeId}/settings`,
       label: "Settings",
       active: pathname === `/${params.storeId}/settings`,
