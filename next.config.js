e511c17ad88217ba96e76baf4095f2a347c0ff4d
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-10 08:25:30 -0700

    added domain for cloudinary

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  image: {
+    domains: ["res.cloudinary.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
