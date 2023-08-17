0cce38a187f65ee149af757a9e7adbf3e036caef
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-16 18:32:57 -0700

    created data table component - shadcn

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,6 +1,9 @@
 import prismadb from "@/lib/prismadb";
 
+import { format } from "date-fns";
+
 import { BillboardClient } from "./components/client";
+import { BillboardColumn } from "./components/columns";
 
 const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
   const billboards = await prismadb.billboard.findMany({
@@ -11,6 +14,13 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
       createdAt: "desc",
     },
   });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
