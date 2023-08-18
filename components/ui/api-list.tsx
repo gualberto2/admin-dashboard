b16e7f4df3651a1534ae0efb6d777baa319dbf5e
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-18 15:08:50 -0700

    api alert styling added for each function type

--- components/ui/api-list.tsx
+++ components/ui/api-list.tsx
@@ -1,5 +1,49 @@
 "use client";
 
-export const ApiList = () => {
-  return <div></div>;
+import { useOrigin } from "@/hooks/new-origin";
+import { useParams } from "next/navigation";
+import { ApiAlert } from "./api-alert";
+
+interface ApiListProps {
+  entityName: string;
+  entityIdName: string;
+}
+
+export const ApiList: React.FC<ApiListProps> = ({
+  entityIdName,
+  entityName,
+}) => {
+  const params = useParams();
+  const origin = useOrigin();
+
+  const baseUrl = `${origin}/api/${params.storeId}`;
+  return (
+    <>
+      <ApiAlert
+        title="GET"
+        variant="public"
+        description={`${baseUrl}/${entityName}`}
+      />
+      <ApiAlert
+        title="GET"
+        variant="public"
+        description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+      />
+      <ApiAlert
+        title="POST"
+        variant="admin"
+        description={`${baseUrl}/${entityName}`}
+      />
+      <ApiAlert
+        title="PATCH"
+        variant="admin"
+        description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+      />
+      <ApiAlert
+        title="DELETE"
+        variant="admin"
+        description={`${baseUrl}/${entityName}/{${entityIdName}}`}
+      />
+    </>
+  );
 };
