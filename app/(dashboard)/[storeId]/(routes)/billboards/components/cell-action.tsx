08ae15cd5f058a1885fadb68f958c50fc1430dcf
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-17 20:57:32 -0700

    cell w actions: edit,update,del

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { BillboardColumn } from "./columns";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+
+interface CellActionProps {
+  data: BillboardColumn;
+}
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Billboard ID copied to clipboard");
+  };
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="h-8 w-8 p-0">
+          <span className="sr-only">Open Menu</span>
+          <MoreHorizontal className="h-4 w-4" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuItem
+          onClick={() =>
+            router.push(`/${params.storeId}/billboards/${data.id}`)
+          }
+        >
+          <Edit className="mr-2 w-4 h-4" /> Update
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => onCopy(data.id)}>
+          <Copy className="mr-2 w-4 h-4" /> Copy ID
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <Trash className="mr-2 w-4 h-4" /> Delete
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
