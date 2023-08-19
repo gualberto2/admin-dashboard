d363a6f0c8e5a7fd5b1dd46aa5b400c851df47c5
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-19 07:59:01 -0700

    fixed an active navbar text bolding issue

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -26,7 +26,7 @@ export function MainNav({
     {
       href: `/${params.storeId}/categories`,
       label: "Categories",
-      active: pathname === `/${params.storeId}/billboards`,
+      active: pathname === `/${params.storeId}/categories`,
     },
     {
       href: `/${params.storeId}/settings`,
