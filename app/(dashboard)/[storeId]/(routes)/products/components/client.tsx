20274e1c1fbe69fbde34ef1699b7a0bcbbcdf738
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-19 21:43:04 -0700

    created products

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface BillboardClientProps {
+  data: Billboard[];
+}
+
+export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Billboards (${data.length})`}
+          description="Manage Billboards for your store"
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/billboards/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading
+        title="API"
+        description="
+      API calls for Billboards"
+      />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
+    </>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/products/page.tsx
@@ -0,0 +1,39 @@
+import prismadb from "@/lib/prismadb";
+
+import { format } from "date-fns";
+
+import { ProductClient } from "./components/client";
+import { ProductColumn } from "./components/columns";
+
+const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
+  const products = await prismadb.product.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      category: true,
+      size: true,
+      color: true.
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+
+  });
+
+  const formattedProducts: ProductColumn[] = products.map((item) => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <ProductClient data={products} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductsPage;
