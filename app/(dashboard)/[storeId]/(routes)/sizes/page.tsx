7905b85ebc6fe63897b78d1c9c2b0b94fc72ecf7
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-19 20:12:26 -0700

    renamed everything to size

--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -2,11 +2,11 @@ import prismadb from "@/lib/prismadb";
 
 import { format } from "date-fns";
 
-import { BillboardClient } from "./components/client";
-import { BillboardColumn } from "./components/columns";
+import { SizesClient } from "./components/client";
+import { SizeColumn } from "./components/columns";
 
-const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
-  const billboards = await prismadb.billboard.findMany({
+const SizesPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.size.findMany({
     where: {
       storeId: params.storeId,
     },
@@ -15,19 +15,20 @@ const BillboardsPage = async ({ params }: { params: { storeId: string } }) => {
     },
   });
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
+  const formattedSizes: SizeColumn[] = billboards.map((item) => ({
     id: item.id,
-    label: item.label,
+    name: item.name,
+    value: item.value,
     createdAt: format(item.createdAt, "MMMM do, yyyy"),
   }));
 
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={billboards} />
+        <SizesClient data={formattedSizes} />
       </div>
     </div>
   );
 };
 
-export default BillboardsPage;
+export default SizesPage;
