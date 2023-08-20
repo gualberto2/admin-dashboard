32f6e39c9666da05f23f4d650cd7b4227e631894
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-19 20:23:47 -0700

    copied colors

---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/cell-action.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { SizeColumn } from "./columns";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import axios from "axios";
+import { AlertModal } from "@/components/modals/alert-modals";
+
+interface CellActionProps {
+  data: SizeColumn;
+}
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const router = useRouter();
+  const params = useParams();
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Size ID copied to clipboard");
+  };
+
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success("Size deleted.");
+    } catch (error: any) {
+      toast.error("Make sure you removed all products using this size first.");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open Menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
+          >
+            <Edit className="mr-2 w-4 h-4" /> Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className="mr-2 w-4 h-4" /> Copy ID
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(open)}>
+            <Trash className="mr-2 w-4 h-4" /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/colors/components/client.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { SizeColumn, columns } from "./columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface SizesClientProps {
+  data: SizeColumn[];
+}
+
+export const SizesClient: React.FC<SizesClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Sizes (${data.length})`}
+          description="Manage Sizes for your store"
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="name" />
+      <Heading
+        title="API"
+        description="
+      API calls for Sizes"
+      />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
+    </>
+  );
+};
