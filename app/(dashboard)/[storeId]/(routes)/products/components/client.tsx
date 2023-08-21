27ef456819933e5736599b5ddd7a637cd2e82fe4
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-20 19:19:56 -0700

    created orders nav section

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -30,7 +30,7 @@ export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="product" />
+      <DataTable columns={columns} data={data} searchKey="name" />
       <Heading
         title="API"
         description="
