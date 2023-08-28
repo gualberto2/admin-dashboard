52aca832bd7630345d683c7e064ebdf99a045da7
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-27 22:17:59 -0700

    provided env key for stripe intergration

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,6 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe();
+export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
+  apiVersion: "2022-11-15",
+  typescript: true,
+});
