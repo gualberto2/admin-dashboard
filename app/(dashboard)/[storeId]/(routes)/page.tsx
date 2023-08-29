0cc1cd3d89525d1b4e11ebfcedbbdea4cab2ddcb
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-29 15:48:49 -0700

    more stylinh

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -12,6 +12,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
       <div className="flex-1 space-y-4 p-8 pt-6">
         <Heading title="Dashboard" description="Overview of your store" />
         <Separator />
+        <div className="grid gap-4 grid-cols-3"></div>
       </div>
     </div>
   );
