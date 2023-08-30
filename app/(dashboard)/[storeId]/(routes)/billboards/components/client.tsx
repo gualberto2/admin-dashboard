df7c764f619b7ddd59d9a6fc5044bde0d94f7d0c
gualberto2 <elijahwilsonnn@gmail.com>
2023-08-30 11:42:41 -0700

    fixed interface call for vercel deployment

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,44 +1,41 @@
 "use client";
 
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Billboard } from "@prisma/client";
-
-import { Plus } from "lucide-react";
-import { useParams, useRouter } from "next/navigation";
-import { columns } from "./columns";
 import { ApiList } from "@/components/ui/api-list";
 
+import { columns, BillboardColumn } from "./columns";
+
 interface BillboardClientProps {
-  data: Billboard[];
+  data: BillboardColumn[];
 }
 
 export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
-  const router = useRouter();
   const params = useParams();
+  const router = useRouter();
+
   return (
     <>
       <div className="flex items-center justify-between">
         <Heading
           title={`Billboards (${data.length})`}
-          description="Manage Billboards for your store"
+          description="Manage billboards for your store"
         />
         <Button
           onClick={() => router.push(`/${params.storeId}/billboards/new`)}
         >
-          <Plus className="mr-2 h-4 w-4" />
-          Add New
+          <Plus className="mr-2 h-4 w-4" /> Add New
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
-      <Heading
-        title="API"
-        description="
-      API calls for Billboards"
-      />
+      <DataTable searchKey="label" columns={columns} data={data} />
+      <Heading title="API" description="API Calls for Billboards" />
+      <Separator />
       <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
